c71fdba5cc9288cb7d944dc45c5c072107a9b12b
Arjunn <92727077+allwinoz@users.noreply.github.com>
2021-10-27 19:39:20 +0900

    Create SendMoneyExample.sol

---
+++ SendMoneyExample.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.1;
+
+contract sendmoneyexample {
+    
+    uint public balanceReceived;
+    uint public lockeduntil;
+    
+    function receiveMoney() public payable {
+        balanceReceived += msg.value; //msg untuk ambil data dari pengirim baik nominal/nama
+        lockeduntil = block.timestamp + 7 seconds;
+    }
+    
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function withdrawMoney() public {
+        if (lockeduntil < block.timestamp) {
+            address payable to = payable(msg.sender);
+            to.transfer(getBalance());
+        }    
+    }
+    
+    function SendMoneyTo (address payable tujuan) public {
+        if (lockeduntil < block.timestamp) {
+            tujuan.transfer(getBalance());
+        }
+    }
+}
